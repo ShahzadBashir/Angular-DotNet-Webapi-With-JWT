6d504dc1852403a83bf1cec89698405c8cdc0873
Shahzad Bashir <shahzadbirmani8@gmail.com>
2021-04-27 02:11:36 +0500

    Initial Push

---
+++ API/API/Controllers/Account.cs
@@ -0,0 +1,24 @@
+﻿using API.DTO_s;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class Account : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/getName")]
+
+
+        public User Name(User _user)
+        {
+            _user.Name = "Shahzad";
+            return _user;
+        }
+
+    }
+}
